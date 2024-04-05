574d9015369547dbee602b9d8ec8531b3ece4acf
Mannar Hielal <mannar.hielal@zurich.ch>
2024-04-05 09:18:29 +0200

    Handling events

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
+import TabButton from "./components/TabButton/TabButton";
 
 function App() {
+  function handleClick(selectedButton) {
+    console.log("----",selectedButton);
+  }
+
   return (
     <div>
       <Header></Header>
@@ -20,6 +25,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
             <CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
           </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={()=> handleClick("components")}>Components</TabButton>
+            <TabButton onSelect={()=> handleClick("jsx")}>JSX</TabButton>
+            <TabButton onSelect={()=> handleClick("props")}>Props</TabButton>
+            <TabButton onSelect={()=> handleClick("state")}>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
